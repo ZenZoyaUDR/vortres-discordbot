81b0be57b064bfa34d3823fe41c486e3251dab39
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-01-16 03:46:27 +0000

    Update purge.js

--- src/commands/pruge.js
+++ src/commands/pruge.js
@@ -1,32 +1,40 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('pruge')
     .setDescription('Delete the last X amount of messages')
-    .addNumberOption(opt => opt.setName('amount').setDescription('Amount of messages to delete').setRequired(true)),
+    .addIntegerOption(option =>
+      option
+        .setName('amount')
+        .setDescription('Amount of messages to delete')
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(99)
+    ),
   async execute(interaction, client) {
     const amount = interaction.options.getInteger('amount');
 
-    if (amount < 1 || amount > 99) {
-      let cmdFormatError = {
-        description: `You only can delete \`1 to 99\` messages at a time.`,
-        color: client.color.fail,
+    if (interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages) || interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      await interaction.channel.bulkDelete(amount, true).catch(err => {
+        console.error('\n\nAn error has occured:\n', err, '\n\n');
+        let cmdError = {
+          description: `There was an error while executing this command!\n\n**Error:**\n\`\`\`${err}\`\`\``,
+          color: client.color.fail,
+        }
+        return interaction.reply({ embeds: [cmdError], ephemeral: true });
+      });
+      let cmdSuc = {
+        description: `Successfully purge \`${amount}\` messages.`,
+        color: client.color.main,
       }
-      return interaction.reply({ embeds: [cmdFormatError], ephemeral: true });
-    }
-    await interaction.channel.bulkDelete(amount, true).catch(err => {
-      console.error('\n\nAn error has occured:\n', err, '\n\n');
-      let cmdError = {
-        description: `There was an error while executing this command!\n\n**Error:**\n\`\`\`${err}\`\`\``,
-        color: client.color.fail,
+      return interaction.reply({ embeds: [cmdSuc] });
+    } else {
+      let cmdSuc = {
+        description: `You need to have permission \`MANAGE_MESSAGES\` to use this command.`,
+        color: client.color.pending,
       }
-      return interaction.reply({ embeds: [cmdError], ephemeral: true });
-    });
-    let cmdSuc = {
-      description: `Successfully purge \`${amount}\` messages.`,
-      color: client.color.fail,
+      return interaction.reply({ embeds: [cmdSuc] });
     }
-    return interaction.reply({ embeds: [cmdSuc] });
-  },
+  }
 };
