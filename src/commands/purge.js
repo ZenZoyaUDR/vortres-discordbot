02eb01157153a52061d9d601d0b7e306d28698c4
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-01-30 21:34:51 +0800

    change in purge.js

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -12,34 +12,29 @@ module.exports = {
         .setMinValue(1)
         .setMaxValue(100)
     ),
+
   async execute(interaction, client) {
     const amount = interaction.options.getInteger('amount');
-    let cmdLoading = {
-      description: `<a:loading:1069476742571511860> Loading...`,
-      color: client.color.yellow,
-    }
-    return interaction.reply({ embeds: [cmdLoading] });
-
     if (interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages) || interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       await interaction.channel.bulkDelete(amount, true).catch(err => {
-        console.error('\n\nAn error has occured:\n', err, '\n\n');
+        console.log('\n\nAn error has occured:\n', err, '\n\n');
         let cmdError = {
           description: `There was an error while executing this command!\n\n**Error:**\n\`\`\`${err}\`\`\``,
           color: client.color.red,
         }
-        return interaction.update({ embeds: [cmdError], ephemeral: true });
+        return interaction.reply({ embeds: [cmdError], ephemeral: true });
       });
       let cmdSuc = {
         description: `Successfully purge \`${amount}\` messages.`,
         color: client.color.blue,
       }
-      return interaction.update({ embeds: [cmdSuc] });
+      return interaction.reply({ embeds: [cmdSuc] });
     } else {
-      let cmdSuc = {
+      let cmdDeny = {
         description: `You need to have permission \`MANAGE_MESSAGES\` to use this command.`,
         color: client.color.yellow,
       }
-      return interaction.update({ embeds: [cmdSuc] });
+      return interaction.reply({ embeds: [cmdDeny] });
     }
   }
 };
