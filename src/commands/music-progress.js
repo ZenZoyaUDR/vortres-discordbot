01df38953eca8ebc4f4d0e15985efb00de03652d
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-02-14 11:44:45 +0800

    Update music-progress.js

--- src/commands/music-progress.js
+++ src/commands/music-progress.js
@@ -9,12 +9,12 @@ module.exports = {
         const queue = player.getQueue(interaction.guild.id);
 
         if (!queue || !queue.playing) {
-            return interaction.reply({ content: `There is no music currently playing!.`, allowedMentions: { repliedUser: false } });
+            return interaction.reply({ content: `There is no music currently playing!`, allowedMentions: { repliedUser: false } });
         }
         const progress = queue.createProgressBar();
         const timestamp = queue.getPlayerTimestamp();
         if (timestamp.progress == 'Infinity') {
-            return interaction.reply({ content: `This song is live streaming, no duration data to display.`, allowedMentions: { repliedUser: false } });
+            return interaction.reply({ content: `Cant not display duration`, allowedMentions: { repliedUser: false } });
         }
 
         let TsEmbed = {
