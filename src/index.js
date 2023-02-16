a46b9f2cac9030a1217728f38b24e21ac38d86dc
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-02-17 00:19:03 +0700

    Big changes (read desc)
    Added/Change:
    - commands now separated/organize
    - make the code looks better
    - and some more

--- src/index.js
+++ src/index.js
@@ -36,11 +36,17 @@ global.player = new Player(
 
   // Registering Slash commands
   const commands = [];
-  const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+  const commandFolders = fs.readdirSync('./src/commands', { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
 
-  for (const file of commandFiles) {
-    const command = require(`../src/commands/${file}`);
-    commands.push(command.data.toJSON());
+  for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+      const command = require(`../src/commands/${folder}/${file}`);
+      commands.push(command.data.toJSON());
+    }
   }
 
   const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
@@ -55,37 +61,40 @@ global.player = new Player(
 
     console.info(`|> Successfully reloaded ${data.length} application (/) commands`);
   } catch (err) {
-    console.info(`\n\nAn error has occured:\n ${err}\n\n`);
+    console.info(`\n\nAn error has occurred:\n ${err}\n\n`);
   }
 
   // Commands Handler
   client.commands = new Collection();
   const commandsPath = path.join(__dirname, 'commands');
-  const commandFiles2 = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-  for (const file of commandFiles2) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    client.commands.set(command.data.name, command);
+  for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, folder, file);
+      const command = require(filePath);
+      client.commands.set(command.data.name, command);
+    }
   }
 
   client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
-
     const command = client.commands.get(interaction.commandName);
-
     if (!command) return;
+
     try {
       await command.execute(interaction, client);
     } catch (err) {
       let slashError = {
         description: `There was an error while executing this command!\n\n**Error:**\n\`\`\`${err}\`\`\``,
         color: client.color.red,
-      }
+      };
       await interaction.reply({ embeds: [slashError], ephemeral: true });
-      console.info(`\n\nAn error has occured:\n ${err}\n\n`);
+      console.info(`\n\nAn error has occurred:\n ${err}\n\n`);
     }
   });
+
 
   // Logging-in bot
   console.info('|> Authenticating with Discord');
