8840103edff2d8e153434f96316fb651c753126e
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-02-22 11:57:18 +0800

    Update colorHelper.js

--- src/helpers/colorHelper.js
+++ src/helpers/colorHelper.js
@@ -1,7 +1,8 @@
 // '0x' is basically the same as '#'
+// so please remove the '#' in your hex color
 module.exports = {
-  main: 0x2f3136,
-  blue: 0x6d33ff,
+  invis: 0x2f3136,
+  blue: 0x5374f7,
   red: 0xff3333,
   yellow: 0xffff64
 }
