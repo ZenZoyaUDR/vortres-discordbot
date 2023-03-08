222138503e62711d838454abe7b53f7e41c40442
ZenZoyaUDR <95112006+ZenZoyaUDR@users.noreply.github.com>
2023-03-08 12:26:04 +0800

    Update stats.js

--- src/commands/public/stats.js
+++ src/commands/public/stats.js
@@ -9,21 +9,12 @@ module.exports = {
 
      async execute(interaction, client) {
           const player = interaction.options.getString('player');
-          let result = await sql.Execute(`SELECT * FROM player_data WHERE name = '${player}'`);
-          if (result.length == 0) return interaction.reply({ content: 'This player doesn\'t exist in the database.' })
+          let result = await sql.Execute(`SELECT * FROM Player_Stats WHERE username = '${player}'`);
+          if (result.length == 0) return interaction.reply({ content: `${player} doesn\'t exist in the database.` })
 
           let statsEmbed = {
-               description: `${player}\'s Stats.`,
-               fields: [{
-                    name: `Level`,
-                    value: `**${result[0].level}**`,
-                    inline: true
-               },
-               {
-                    name: `XP`,
-                    value: `**${result[0].xp}**`,
-                    inline: true
-               }],
+               title: `${player}\'s Stats`,
+               description: `**Level:** ${result[0].level}\n**XP:** ${result[0].exp}/${result[0].maxExp}`,
                color: client.color.blue,
           }
           interaction.reply({ content: '', embeds: [statsEmbed] });
